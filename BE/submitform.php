4344d3850c75c3ee941ed9572717388b80c7ebb2
Hadi Al Hassan <hadialhassan90@gmail.com>
2024-05-07 12:09:24 +0300

    Contact form now Works with Backend and inserts into DB

--- BE/submitform.php
+++ BE/submitform.php
@@ -7,22 +7,29 @@ require_once("dbconfig.php");
 $form = new stdClass();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $phonenumber = $_POST["phonenumber"];
-    $email = $_POST["email"];
-    $subject = $_POST["subject"];
-    $body = $_POST["message"];
+    $form->phonenumber = $_POST["phonenumber"];
+    $form->email = $_POST["email"];
+    $form->subject = $_POST["subject"];
+    $form->body = $_POST["message"];
 
-    echo $phonenumber.$email.$subject.$body;
-
-
+    InsertFORMToDBFromObject($form);
 }
 
 
+function InsertFORMToDBFromObject($form){
 
+    global $db;
+    try {
 
-
-
-
+        $query = "INSERT INTO table_contacts (PHONE, EMAIL, SUBJECT, BODY) VALUES (?, ?, ?, ?)";
+        $stmt = $db->prepare($query);
+        $stmt->execute([$form->phonenumber, $form->email, $form->subject, $form->body]);
+         
+    } catch (PDOException $e) {
+        print "Error!" . $e->getMessage() . "<br/>";
+        die();
+    }
+}
 
 
 ?>
