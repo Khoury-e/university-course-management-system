73a8a312064a07f1e35ef8d70f96285e68c89c0c
Hadi Al Hassan <hadialhassan90@gmail.com>
2024-05-07 12:25:27 +0300

    Submit form is now fully functional and displays message

--- BE/submitform.php
+++ BE/submitform.php
@@ -1,4 +1,3 @@
-
 <?php
 
 session_start();
@@ -12,7 +11,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $form->subject = $_POST["subject"];
     $form->body = $_POST["message"];
 
-    InsertFORMToDBFromObject($form);
+    if (InsertFORMToDBFromObject($form)) {
+        // Redirect to index.html with a success parameter in the URL
+        header("Location: ../index.html?success=true");
+        exit(); // Stop further execution of the script
+    } else {
+        // If insertion fails, redirect to index.html with an error parameter in the URL
+        header("Location: ../index.html?error=true");
+        exit(); // Stop further execution of the script
+    }
+
 }
 
 
@@ -20,16 +28,17 @@ function InsertFORMToDBFromObject($form){
 
     global $db;
     try {
-
         $query = "INSERT INTO table_contacts (PHONE, EMAIL, SUBJECT, BODY) VALUES (?, ?, ?, ?)";
         $stmt = $db->prepare($query);
         $stmt->execute([$form->phonenumber, $form->email, $form->subject, $form->body]);
+
+        return true;
          
     } catch (PDOException $e) {
+        // Print error message if insertion fails
         print "Error!" . $e->getMessage() . "<br/>";
-        die();
+        return false; // Return false if insertion fails
     }
 }
-
 
 ?>
