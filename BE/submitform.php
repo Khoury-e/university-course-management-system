353811db80ad163f14218dd12fc5a5f0a1ccca8c
Hadi Al Hassan <hadialhassan90@gmail.com>
2024-05-07 11:52:44 +0300

    updated contact us, doing BE for contact form

--- BE/submitform.php
+++ BE/submitform.php
@@ -1,6 +1,25 @@
 
 <?php
 
+session_start();
+require_once("dbconfig.php");
+
+$form = new stdClass();
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $phonenumber = $_POST["phonenumber"];
+    $email = $_POST["email"];
+    $subject = $_POST["subject"];
+    $body = $_POST["message"];
+
+    echo $phonenumber.$email.$subject.$body;
+
+
+}
+
+
+
+
 
 
 
